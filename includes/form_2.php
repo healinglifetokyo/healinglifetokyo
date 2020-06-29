e4d7b8cac42733bbd40c716794c2be91c4e092cb
healinglifetokyo <67537142+healinglifetokyo@users.noreply.github.com>
2020-06-29 22:59:35 +0900

    Add files via upload

--- includes/form_2.php
+++ includes/form_2.php
@@ -8,16 +8,15 @@
 	$email = $_POST['email'];
 	$email_3975 = $_POST['email_3975'];
 	$message = $_POST['message'];
-	$optin = $_POST['optin'];
 	
-	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+	$to = 'healinglifetokyo.reiki@gmail.com'; // Email submissions are sent to this email
 
 	// Create email	
-	$email_subject = "Message from a Blocs website.";
+	$email_subject = "MESSAGE from Healing life ";
 	$email_body = "You have received a new message. \n\n".
-				  "Name: $name \nEmail: $email \nEmail_3975: $email_3975 \nMessage: $message \nOptin: $optin \n";
+				  "Name: $name \nEmail: $email \nEmail_3975: $email_3975 \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: contact@yoursite.com\n";
+	$headers .= "From: healinglifetokyo.reiki@gmail.com\n";
 	$headers .= "Reply-To: $email";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
